9cce3df86ab14374b5b03494b61df46df72362d2
Andrei Andreev <Razenpok@outlook.com>
2020-10-06 09:06:27 +0300

    WIP

--- Razensoft.Trade.Pine.Tests/MainTest.cs
+++ Razensoft.Trade.Pine.Tests/MainTest.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Antlr4.Runtime;
-using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace Razensoft.Trade.Pine.Parsing.Tests
@@ -13,22 +7,7 @@ namespace Razensoft.Trade.Pine.Parsing.Tests
         [Test]
         public void Test1()
         {
-            var scriptSource = File.ReadAllText("example - Processed.pine");
-            //var strategy = PineStrategy.FromScript(scriptSource);
-            //var preprocessed2 = PineScriptPreprocessor.Preprocess(scriptSource);
-            //Console.WriteLine(preprocessed2);
-            //return;
-            var preprocessed = scriptSource
-                .Replace("\r\n", "\n");
-            var inputStream = new AntlrInputStream(preprocessed);
-            var lexer = new PineScriptLexer(inputStream);
-            var tokens = new CommonTokenStream(lexer);
-            var parser = new PineScriptParser(tokens);
-            
-            var classVisitor = new PineScriptVisitor();
-            var statements = classVisitor.VisitScript(parser.script()).ToList();
-            var script = new PineScript(statements);
-
+            var script = PineScript.FromFile("example.pine");
             var executionContext = new RootPineScriptExecutionContext(new DefaultBuiltinProvider());
             script.Execute(executionContext);
         }
--- Razensoft.Trade.Pine/PineScript.cs
+++ Razensoft.Trade.Pine/PineScript.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Antlr4.Runtime;
 using Razensoft.Trade.Pine.Statements;
 
 [assembly: CLSCompliant(false)]
@@ -19,6 +22,19 @@ namespace Razensoft.Trade.Pine.Parsing
         {
             var context = new PineScriptExecutionContext(parent);
             _main.Execute(context, new object[0], new Dictionary<string, object>());
+        }
+
+        public static PineScript FromFile(string path)
+        {
+            var scriptSource = File.ReadAllText(path);
+            var preprocessed = PineScriptPreprocessor.Preprocess(scriptSource);
+            var inputStream = new AntlrInputStream(preprocessed);
+            var lexer = new PineScriptLexer(inputStream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new PineScriptParser(tokens);
+            var classVisitor = new PineScriptVisitor();
+            var statements = classVisitor.VisitScript(parser.script()).ToList();
+            return new PineScript(statements);
         }
     }
 }
