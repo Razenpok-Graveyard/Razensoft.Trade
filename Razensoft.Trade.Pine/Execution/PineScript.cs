a5458b68ab3aa9eb9bcf8c51ee0be46897ecad31
Andrei Andreev <Razenpok@outlook.com>
2020-10-08 18:55:30 +0300

    WIP

---
+++ Razensoft.Trade.Pine/Execution/PineScript.cs
@@ -0,0 +1,26 @@
+﻿namespace Razensoft.Trade.Pine.Execution
+{
+    public class PineScript
+    {
+        
+    }
+
+    public abstract class PineScriptInstruction
+    {
+        
+    }
+    
+
+    public abstract class PineScriptVisitor : PineScriptBaseVisitor<PineScript>
+    {
+    }
+    
+
+    public abstract class PineScriptInstructionVisitor : PineScriptBaseVisitor<PineScriptInstructionVisitor>
+    {
+        public virtual void A()
+        {
+            
+        }
+    }
+}
